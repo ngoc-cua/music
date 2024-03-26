b0be4f1f70186218cd38fd9610467835dd4537e7
ngoc-cua <nguyenthingoccua2004@gmail.com>
2024-03-26 14:18:38 +0700

    init music-player

---
+++ player.js
@@ -0,0 +1,12 @@
+
+Amplitude.init({
+    songs: [
+        {
+            name: 'Đợi chờ không là hạnh phúc',
+            artist: 'Lương Thế Minh',
+            album: 'Đợi chờ không là hạnh phúc',
+            url: './music/Đợi chờ là hạnh phúc.mp3',
+            cover_art_url: './music/meodoi.jpg'
+        }
+    ]
+});
